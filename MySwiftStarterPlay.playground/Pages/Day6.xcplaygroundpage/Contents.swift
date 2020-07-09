7f8cdaf4f31af0d99d5f142bb1a2a2d33a5ee322
hirvin-faria <hirvin.nogueira.faria@gmail.com>
2020-07-09 20:58:07 -0300

    Day 6

---
+++ MySwiftStarterPlay.playground/Pages/Day6.xcplaygroundpage/Contents.swift
@@ -0,0 +1,51 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//DAY 6 - Closures
+
+let driving = {
+    print("I'm driving in my car")
+}
+
+driving()
+
+
+let drivingWithParm = { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+
+drivingWithParm("Berlin")
+
+
+let drivingWithReturn = { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+let message = drivingWithReturn("Berlin")
+print(message)
+
+
+// Closures como parametros de funções
+func travel(action: () -> Void){
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+
+travel(action: driving)
+
+
+
+
+//Traling Closure Syntax
+func travel2(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+travel {
+    print("I'm driving in my car")
+}
+
+//: [Next](@next)
