6df2c11e0d7837f6f31c721564384615c5f14963
hirvin-faria <hirvin.nogueira.faria@gmail.com>
2020-07-16 22:18:02 -0300

    :sparkles: Day 11

---
+++ MySwiftStarterPlay.playground/Pages/Day11.xcplaygroundpage/Contents.swift
@@ -0,0 +1,67 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// DAY 11 - Protocol
+
+protocol Identifiable{
+    var id: String { get set }
+}
+
+struct User: Identifiable {
+    var id: String
+}
+
+func displayID(thing: Identifiable) {
+    print("My ID is \(thing.id)")
+}
+
+
+// inheritance
+
+protocol Payable {
+    func calculateWages() -> Int
+}
+
+protocol NeedsTraining {
+    func study()
+}
+
+protocol HasVacation {
+    func takeVacation(days: Int)
+}
+
+protocol Employee: Payable, NeedsTraining, HasVacation { }
+
+
+// extension
+// cria metodos personalizados para os tipos padrões do swift
+extension Int {
+    func squared() -> Int {
+        return self * self
+    }
+}
+
+let number = 8
+number.squared()
+
+
+// extension protocol
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+let beatles = Set(["John", "Paul", "George", "Ringo"])
+
+extension Collection {
+    func summarize() {
+        print("There are \(count) of us:")
+
+        for name in self {
+            print(name)
+        }
+    }
+}
+
+pythons.summarize()
+beatles.summarize()
+
+
+//: [Next](@next)
