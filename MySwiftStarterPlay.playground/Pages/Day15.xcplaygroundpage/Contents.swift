839a93f535291b2c8d2320efd603814bf58b6560
hirvin-faria <hirvin.nogueira.faria@gmail.com>
2020-07-27 20:27:07 -0300

    :sparkles: Day 15

---
+++ MySwiftStarterPlay.playground/Pages/Day15.xcplaygroundpage/Contents.swift
@@ -0,0 +1,103 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+// Struct Properties
+struct Person {
+    var clothes: String {
+        willSet {
+            updateUI(msg: "I'm changing from \(clothes) to \(newValue)")
+        }
+
+        didSet {
+            updateUI(msg: "I just changed from \(oldValue) to \(clothes)")
+        }
+    }
+}
+
+func updateUI(msg: String) {
+    print(msg)
+}
+
+var taylor = Person(clothes: "T-shirts")
+taylor.clothes = "short skirts"
+
+
+// Static Methods and Properties
+struct TaylorFan {
+    static var favoriteSong = "Look What You Made Me Do"
+
+    var name: String
+    var age: Int
+}
+
+let fan = TaylorFan(name: "James", age: 25)
+print(TaylorFan.favoriteSong)
+
+// Acess Control
+// Public
+// Internal
+// File Private
+// Private
+class TaylorSuperFan {
+    private var nome: String?
+    fileprivate let idade: Int?
+    
+    init(nome: String, idade: Int) {
+        self.nome = nome
+        self.idade = idade
+    }
+}
+
+
+
+class Album {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+
+    func getPerformance() -> String {
+        return "The album \(name) sold lots"
+    }
+}
+
+class StudioAlbum: Album {
+    var studio: String
+
+    init(name: String, studio: String) {
+        self.studio = studio
+        super.init(name: name)
+    }
+
+    override func getPerformance() -> String {
+        return "The studio album \(name) sold lots"
+    }
+}
+
+class LiveAlbum: Album {
+    var location: String
+
+    init(name: String, location: String) {
+        self.location = location
+        super.init(name: name)
+    }
+
+    override func getPerformance() -> String {
+        return "The live album \(name) sold lots"
+    }
+}
+
+var taylorSwift = StudioAlbum(name: "Taylor Swift", studio: "The Castles Studios")
+var fearless = StudioAlbum(name: "Speak Now", studio: "Aimeeland Studio")
+var iTunesLive = LiveAlbum(name: "iTunes Live from SoHo", location: "New York")
+
+var allAlbums: [Album] = [taylorSwift, fearless, iTunesLive]
+
+for album in allAlbums {
+    print(album.getPerformance())
+}
+
+//: [Next](@next)
